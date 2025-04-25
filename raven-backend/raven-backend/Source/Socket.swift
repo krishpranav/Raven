ddfe1b5cd37ab908840957f9df116606cf521b2d
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-25 18:21:24 +0530

    raven-backend: Source[Socket]

--- raven-backend/raven-backend/Source/Socket.swift
+++ raven-backend/raven-backend/Source/Socket.swift
@@ -9,7 +9,7 @@
  */
 
 #if os(Linux)
-import GLibc
+import Glibc
 #else
 import Darwin
 #endif
@@ -33,15 +33,30 @@ extension SocketType {
     }
 }
 
-
 struct Socket {
     var type: SocketType
     var raw: Int32
 
     init(raw: Int32, type: SocketType = .tcp) throws {
+        let flags = fcntl(raw, F_GETFL, 0)
+        let status = fcntl(raw, F_SETFL, flags);
+
+        if raw < 0 {
+            throw SocketError.NoFileDescriptor
+        }
+
+        if status < 0 {
+            close(raw)
+            throw SocketError.NoFileDescriptor
+        }
+
+        self.raw = raw
+        self.type = type
     }
 
     init(type: SocketType = .tcp) throws {
+        let fd = socket(AF_INET, type.sockValue, 0)
+        try self.init(raw: fd, type: type)
     }
 
     func shutdown() {
