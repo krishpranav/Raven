9ea6307ec75667ef7efab76902e6c641230745a9
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-25 14:51:51 +0530

    raven-backend

---
+++ raven-backend/raven-backend/raven_backend.h
@@ -0,0 +1,18 @@
+//
+//  raven_backend.h
+//  raven-backend
+//
+//  Created by Krisna Pranav on 25/04/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for raven_backend.
+FOUNDATION_EXPORT double raven_backendVersionNumber;
+
+//! Project version string for raven_backend.
+FOUNDATION_EXPORT const unsigned char raven_backendVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <raven_backend/PublicHeader.h>
+
+
