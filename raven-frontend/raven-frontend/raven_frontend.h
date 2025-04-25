2c4cd5a9bbf880a4e192edea93b68891f2363175
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-25 14:53:08 +0530

    raven-frontend

---
+++ raven-frontend/raven-frontend/raven_frontend.h
@@ -0,0 +1,18 @@
+//
+//  raven_frontend.h
+//  raven-frontend
+//
+//  Created by Krisna Pranav on 25/04/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for raven_frontend.
+FOUNDATION_EXPORT double raven_frontendVersionNumber;
+
+//! Project version string for raven_frontend.
+FOUNDATION_EXPORT const unsigned char raven_frontendVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <raven_frontend/PublicHeader.h>
+
+
