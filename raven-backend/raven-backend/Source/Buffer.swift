9165e02a8871f38852cc36fd3d5e02904490ca59
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-27 19:26:43 +0530

    raven-backend: Source[Buffer]

--- raven-backend/raven-backend/Source/Buffer.swift
+++ raven-backend/raven-backend/Source/Buffer.swift
@@ -8,6 +8,7 @@
  *
  */
 
+
 import Foundation
 
 public class Buffer {
@@ -27,20 +28,39 @@ public class Buffer {
         self.allocated = false
     }
 
-    init(size: Int) {
-        self.size = size
-        self.buffer = UnsafeMutableRawPointer.allocate(bytes: .size, alignedTo: 1)
+    init(data: Foundation.Data) {
+        var bytes: [UInt8] = []
+
+        data.withUnsafeBytes {
+            bytes = [UInt8](UnsafeBufferPointer(start: $0, count: data.count))
+        }
+
+        self.bytes = bytes
+        self.size = data.count
+
+        let raw = UnsafeMutablePointer<UInt8>(mutating: self.bytes)
+
+        self.buffer = UnsafeMutableRawPointer(raw)
+        self.allocated = false
     }
 
-    deint {
+    init(size: Int) {
+        self.size = size
+        self.buffer = UnsafeMutableRawPointer.allocate(bytes: size, alignedTo: 1)
+        self.allocated = true
+    }
+
+    deinit {
         if (self.allocated) {
             self.buffer.deallocate(bytes: size, alignedTo: 1)
         }
     }
 
     func toString() -> String {
-        let string = String()
-
+        let string = String(bytesNoCopy: buffer, length: size,
+                            encoding: String.Encoding.utf8,
+                            freeWhenDone: false) ?? ""
         return string
     }
+
 }
