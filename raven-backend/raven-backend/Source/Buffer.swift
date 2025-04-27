2452d8d484f24dd84a6f855cd83fac484089bfa4
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-27 19:25:03 +0530

    raven-backend: Source[Buffer]

---
+++ raven-backend/raven-backend/Source/Buffer.swift
@@ -0,0 +1,29 @@
+/**
+ * @file Buffer.swift
+ * @author Krisna Pranav
+ * @version 1.0
+ * @date 2025-04-25
+ *
+ * @copyright Copyright (c) 2025 Krisna Pranav
+ *
+ */
+
+import Foundation
+
+public class Buffer {
+    let size: Int
+    let buffer: UnsafeMutableRawPointer
+    let allocated: Bool
+
+    private var bytes: [UInt8] = []
+
+    init(string: String) {
+        self.bytes = [UInt8](string.utf8)
+
+        let bytes = UnsafeMutablePointer<UInt8>(mutating: self.bytes)
+
+        self.buffer = UnsafeMutableRawPointer(bytes)
+        self.size = self.bytes.count
+        self.allocated = false
+    }
+}
