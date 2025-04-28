49d0946680b52285ba0850f109d0f0eb12883dda
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-28 19:22:05 +0530

    raven-backend: Source[HTTPMethod]

--- raven-backend/raven-backend/Source/HTTPMethod.swift
+++ raven-backend/raven-backend/Source/HTTPMethod.swift
@@ -11,16 +11,21 @@
 import Foundation
 
 public enum HTTPMethod: String {
-    case get = "GET"
-    case post = "POST"
+  case get = "GET"
+  case post = "POST"
+  case put = "PUT"
+  case delete = "DELETE"
+  case patch = "PATCH"
+  case unknown = "UNKNOWN"
 }
 
 extension HTTPMethod {
-    init?(string: String) {
-        if let method = HTTPMethod.init(rawValue: string) {
-            self = method
-        } else {
-            return nil
-        }
+
+  init?(string: String) {
+    if let method = HTTPMethod.init(rawValue: string) {
+      self = method
+    } else {
+      return nil
     }
+  }
 }
