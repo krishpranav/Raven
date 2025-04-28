af5caf2aff26ac352294de1fd009bbe01d904961
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-28 19:21:36 +0530

    raven-backend: Source[HTTPMethod]

--- raven-backend/raven-backend/Source/HTTPMethod.swift
+++ raven-backend/raven-backend/Source/HTTPMethod.swift
@@ -1,8 +1,26 @@
-//
-//  HTTPMethod.swift
-//  raven-backend
-//
-//  Created by Krisna Pranav on 28/04/25.
-//
+/**
+ * @file HTTPMethod.swift
+ * @author Krisna Pranav
+ * @version 1.0
+ * @date 2025-04-25
+ *
+ * @copyright Copyright (c) 2025 Krisna Pranav
+ *
+ */
 
 import Foundation
+
+public enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+}
+
+extension HTTPMethod {
+    init?(string: String) {
+        if let method = HTTPMethod.init(rawValue: string) {
+            self = method
+        } else {
+            return nil
+        }
+    }
+}
